84e58201a13e656baaab3592da789159189be889
Punk_UnDeaD <punkundead@gmail.com>
2016-06-12 01:31:52 +0300

    удалить неиспользуемые библиотеки

--- sites/all/libraries/jquery.placeholder/placeholder.js
+++
@@ -1,7 +0,0 @@
-(function ($) {
-  Drupal.behaviors.placeholder = {
-    attach:function (context, settings) {
-      $('input[placeholder],textarea[placeholder]', context).placeholder();
-    }
-  };
-}(jQuery));
